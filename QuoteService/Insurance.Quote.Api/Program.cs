92c4df2222f04091796953753924e99b40684ec8
Fraanst <fran.stran@gmail.com>
2025-11-18 21:16:04 -0300

    feat: adicionado swagger

--- QuoteService/Insurance.Quote.Api/Program.cs
+++ QuoteService/Insurance.Quote.Api/Program.cs
@@ -1,17 +1,27 @@
+using Insurance.Quote.Application;
+using Insurance.Quote.Infrastructure;
+using Microsoft.OpenApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddInfrastructure(); 
+builder.Services.AddApplication();
+
+builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "API Proposta Service", Version = "v1" });
+    });
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
-
-
+app.MapControllers();
 app.Run();
 
