fe1fd8cb6caa3d781ce431e8e1dc9367238be5c0
Fraanst <fran.stran@gmail.com>
2025-11-19 09:56:59 -0300

    feat: adicionado migrations

---
+++ QuoteService/Insurance.Quote.Infrastructure/Migrations/20251119125212_InitialQuoteSchema.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Insurance.Quote.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialQuoteSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    customer_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CustomerName = table.Column<string>(type: "character varying(250)", maxLength: 250, nullable: false),
+                    DocumentNumber = table.Column<string>(type: "text", nullable: true),
+                    BirthDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.customer_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    product_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ProductType = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Value = table.Column<decimal>(type: "numeric(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.product_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Quotes",
+                columns: table => new
+                {
+                    quote_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CustomerId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ProductId = table.Column<Guid>(type: "uuid", nullable: false),
+                    InsuranceType = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
+                    Status = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    EstimatedValue = table.Column<decimal>(type: "numeric(18,2)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    CustomerEntityCustomerId = table.Column<Guid>(type: "uuid", nullable: true),
+                    ProductEntityProductId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Quotes", x => x.quote_id);
+                    table.ForeignKey(
+                        name: "FK_Quotes_Customers_CustomerEntityCustomerId",
+                        column: x => x.CustomerEntityCustomerId,
+                        principalTable: "Customers",
+                        principalColumn: "customer_id");
+                    table.ForeignKey(
+                        name: "FK_Quotes_Customers_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customers",
+                        principalColumn: "customer_id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Quotes_Products_ProductEntityProductId",
+                        column: x => x.ProductEntityProductId,
+                        principalTable: "Products",
+                        principalColumn: "product_id");
+                    table.ForeignKey(
+                        name: "FK_Quotes_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "product_id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_DocumentNumber",
+                table: "Customers",
+                column: "DocumentNumber",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Quotes_CustomerEntityCustomerId",
+                table: "Quotes",
+                column: "CustomerEntityCustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Quotes_CustomerId",
+                table: "Quotes",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Quotes_ProductEntityProductId",
+                table: "Quotes",
+                column: "ProductEntityProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Quotes_ProductId",
+                table: "Quotes",
+                column: "ProductId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Quotes");
+
+            migrationBuilder.DropTable(
+                name: "Customers");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
